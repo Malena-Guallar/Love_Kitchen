fb574338675a391f40e1c480481e93c1ac0048b2
Malena Guallar <malena.guallar@gmail.com>
2023-05-21 11:37:14 +0200

    hiding form when clicking on send + style

--- add.php
+++ add.php
@@ -16,34 +16,8 @@
         <?php include_once('header.php'); ?>
     </header> 
 
-    <div class="add_container">
-        <form class="recipe_adding" method="post">
-
-            <h2>add a recipe below</h2>
-            <br/>
-            <div class="name">
-                <label>your name</label>
-                <input type="name" id="name" class="name_input" name="name"/>
-            </div>
-            <br/>
-            <div class="title">
-                <label>title of recipe</label>
-                <input type="title" id="title" class="title_input" name="title"/>
-            </div>
-            <br/>
-            <div class="recipe">
-                <label>your recipe</label>
-                <textarea id="recipe" class="recipe_area" name="recipe" rows="10" cols="50"></textarea>
-            </div>
-            </br>
-            <div class="btn_container">
-                <button class="btn" type="submit">send</button>
-            </div>
-        </form>
-    </div>
-
-
     <?php
+        $formSubmitted = false; 
 
         if (isset($_POST['name']) && isset($_POST['title']) && isset($_POST['recipe']) && !empty($_POST['name']) && !empty($_POST['title']) && !empty($_POST['recipe'])) {
             
@@ -57,13 +31,46 @@
                 'recipe' => $recipe,
                 'author' => $name 
             ]);
-
+            $formSubmitted = true;
 
         } else {
             $errorMessage = 'Veuillez remplir tous les champs svp' ;
         }
 
         ?>
+
+        <?php if (!$formSubmitted): ?>
+
+    <div class="add_container">
+        <form class="recipe_adding" method="post">
+
+            <h2>add a recipe below</h2>
+            <br/>
+            <div class="name">
+                <label>your name</label>
+                <br>
+                <input type="name" id="name" class="name_input" name="name"/>
+            </div>
+            <br/>
+            <div class="title">
+                <label>title of recipe</label>
+                <br>
+                <input type="title" id="title" class="title_input" name="title"/>
+            </div>
+            <br/>
+            <div class="recipe">
+                <label>your recipe</label>
+                <br>
+                <textarea id="recipe" class="recipe_area" name="recipe" rows="10" cols="50"></textarea>
+            </div>
+            </br>
+            <div class="btn_container">
+                <button class="btn" type="submit">send</button>
+            </div>
+        </form>
+    </div>
+
+    <?php endif; ?>
 
 
 <?php if (isset($errorMessage)): ?>
@@ -72,7 +79,7 @@
     </div>
 <?php endif; ?>
 
-<?php if (isset($_POST['title']) && isset($_POST['recipe']) && !empty($_POST['title']) && !empty($_POST['recipe']) && !isset($errorMessage)): ?>
+<?php if (isset($title) && isset($recipe) && !empty($title) && !empty($recipe) && !isset($errorMessage)): ?>
     <div class="alert_container">
         <div class="alert_success" role="alert">
             <p>♥ ♥ ♥</p>
